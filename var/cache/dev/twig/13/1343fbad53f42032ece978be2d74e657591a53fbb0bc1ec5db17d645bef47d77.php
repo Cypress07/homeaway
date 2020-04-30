07c6e6f3e550849fd19c1261cfa47f7a442a94a8
m.ouzera@free.fr <m.ouzera@free.fr>
2020-04-30 10:29:55 +0200

    update v1.9 run bundle

--- var/cache/dev/twig/13/1343fbad53f42032ece978be2d74e657591a53fbb0bc1ec5db17d645bef47d77.php
+++ var/cache/dev/twig/13/1343fbad53f42032ece978be2d74e657591a53fbb0bc1ec5db17d645bef47d77.php
@@ -60,11 +60,14 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 
 \t\t<link href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">
 \t\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/css/select2.min.css\">
-
+\t\t";
+        // line 14
+        echo $this->extensions['Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension']->renderWebpackLinkTags("app");
+        echo "
 \t\t";
         // line 15
         $this->displayBlock('stylesheets', $context, $blocks);
-        // line 18
+        // line 17
         echo "\t\t</head>
 
 \t\t<body>
@@ -72,7 +75,7 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 \t\t\t<nav class=\"navbar navbar-expand-lg navbar-dark bg-primary\">
 
 \t\t\t\t<a class=\"navbar-brand\" href=\"";
-        // line 24
+        // line 23
         echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("home");
         echo "\">HomeAway</a>
 
@@ -90,8 +93,8 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 \t\t\t\t\t\t<li class=\"nav-item\">
 
 \t\t\t\t\t\t\t<a class=\"nav-link ";
-        // line 39
-        if (((isset($context["current_menu"]) || array_key_exists("current_menu", $context)) && 0 === twig_compare((isset($context["current_menu"]) || array_key_exists("current_menu", $context) ? $context["current_menu"] : (function () { throw new RuntimeError('Variable "current_menu" does not exist.', 39, $this->source); })()), "properties"))) {
+        // line 38
+        if (((isset($context["current_menu"]) || array_key_exists("current_menu", $context)) && 0 === twig_compare((isset($context["current_menu"]) || array_key_exists("current_menu", $context) ? $context["current_menu"] : (function () { throw new RuntimeError('Variable "current_menu" does not exist.', 38, $this->source); })()), "properties"))) {
             echo "active ";
         }
         echo "\" href=\"";
@@ -102,37 +105,40 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 
 \t\t\t\t\t</ul>
 \t\t\t\t\t";
-        // line 44
-        if (twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 44, $this->source); })()), "user", [], "any", false, false, false, 44)) {
-            // line 45
+        // line 43
+        if (twig_get_attribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 43, $this->source); })()), "user", [], "any", false, false, false, 43)) {
+            // line 44
             echo "\t\t\t\t\t\t<ul class=\"navbar-nav\">
 \t\t\t\t\t\t\t<li class=\"nav-item\">
 \t\t\t\t\t\t\t\t<a href=\"";
-            // line 47
+            // line 46
             echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("logout");
             echo "\" class=\"nav-link\">Se déconnecter</a>
 \t\t\t\t\t\t\t</li>
 \t\t\t\t\t\t</ul>
 \t\t\t\t\t";
         }
-        // line 51
+        // line 50
         echo "\t\t\t\t</div>
 
 \t\t\t</nav>
 \t\t\t";
-        // line 54
+        // line 53
         $this->displayBlock('body', $context, $blocks);
-        // line 55
+        // line 54
         echo "
 \t\t\t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js\"></script>
 \t\t\t<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>
 \t\t\t<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>
 
-
+\t\t\t\t";
+        // line 59
+        echo $this->extensions['Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension']->renderWebpackScriptTags("app");
+        echo "
 \t\t\t";
-        // line 61
+        // line 60
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 64
+        // line 62
         echo "\t\t</body>
 \t</html>
 ";
@@ -174,10 +180,7 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
         // line 16
-        echo "\t\t\t";
-        echo $this->extensions['Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension']->renderWebpackLinkTags("app");
-        echo "
-\t\t";
+        echo "\t\t";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
@@ -186,7 +189,7 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 
     }
 
-    // line 54
+    // line 53
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -204,7 +207,7 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 
     }
 
-    // line 61
+    // line 60
     public function block_javascripts($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -214,11 +217,8 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
 
-        // line 62
-        echo "\t\t\t\t";
-        echo $this->extensions['Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension']->renderWebpackScriptTags("app");
-        echo "
-\t\t\t";
+        // line 61
+        echo "\t\t\t";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
 
@@ -239,7 +239,7 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 
     public function getDebugInfo()
     {
-        return array (  218 => 62,  208 => 61,  190 => 54,  177 => 16,  167 => 15,  148 => 9,  136 => 64,  134 => 61,  126 => 55,  124 => 54,  119 => 51,  112 => 47,  108 => 45,  106 => 44,  94 => 39,  76 => 24,  68 => 18,  66 => 15,  59 => 10,  57 => 9,  47 => 1,);
+        return array (  221 => 61,  211 => 60,  193 => 53,  183 => 16,  173 => 15,  154 => 9,  142 => 62,  140 => 60,  136 => 59,  129 => 54,  127 => 53,  122 => 50,  115 => 46,  111 => 44,  109 => 43,  97 => 38,  79 => 23,  71 => 17,  69 => 15,  65 => 14,  59 => 10,  57 => 9,  47 => 1,);
     }
 
     public function getSourceContext()
@@ -257,9 +257,8 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 
 \t\t<link href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">
 \t\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/css/select2.min.css\">
-
+\t\t{{ encore_entry_link_tags('app') }}
 \t\t{% block stylesheets %}
-\t\t\t{{ encore_entry_link_tags('app') }}
 \t\t{% endblock %}
 \t\t</head>
 
@@ -303,9 +302,8 @@ class __TwigTemplate_c5eeac281080bde9deae50c7330540d12490faaa892179063541593d010
 \t\t\t<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>
 \t\t\t<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>
 
-
-\t\t\t{% block javascripts %}
 \t\t\t\t{{ encore_entry_script_tags('app') }}
+\t\t\t{% block javascripts %}
 \t\t\t{% endblock %}
 \t\t</body>
 \t</html>
